4d9ebae2d3d5ab2b14c60075a85235f78586d722
cruth <ceruth@epfc.eu>
2021-07-23 22:52:59 +0200

    Relation ManyToMany between User & Role

--- src/main/java/be/iccbxl/pid/model/Role.java
+++ src/main/java/be/iccbxl/pid/model/Role.java
@@ -1,9 +1,15 @@
 package be.iccbxl.pid.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
 
@@ -14,6 +20,13 @@ public class Role {
 	@GeneratedValue(strategy=GenerationType.AUTO)
 	private Long id;
 	private String role;
+	
+	@ManyToMany
+	@JoinTable(
+		  name = "user_role", 
+		  joinColumns = @JoinColumn(name = "role_id"), 
+		  inverseJoinColumns = @JoinColumn(name = "user_id"))
+	private List<User> users = new ArrayList<>();
 	
 	protected Role() {	}
 	
@@ -32,6 +45,28 @@ public class Role {
 
 	public void setRole(String role) {
 		this.role = role;
+	}
+
+	public List<User> getUsers() {
+		return users;
+	}
+
+	public Role addUser(User user) {
+		if(!this.users.contains(user)) {
+			this.users.add(user);
+			user.addRole(this);
+		}
+		
+		return this;
+	}
+	
+	public Role removeRole(User user) {
+		if(this.users.contains(user)) {
+			this.users.remove(user);
+			user.getRoles().remove(this);
+		}
+		
+		return this;
 	}
 
 	@Override
